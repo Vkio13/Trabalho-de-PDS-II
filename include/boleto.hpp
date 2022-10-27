c4254ac3d9a7abeee7546e38294be729323d2679
GabrielCaldeiraBicalho <69369861+GabrielCaldeiraBicalho@users.noreply.github.com>
2022-10-27 09:37:08 -0300

    Declarações do boleto.hpp

--- include/boleto.hpp
+++ include/boleto.hpp
@@ -1,3 +1,15 @@
-#ifndef
-
+#ifndef BOLETO_H
+#define BOLETO_H
+class Boleto {
+private:
+  int vencimento;
+  bool venceu;
+public:
+  Boleto(int vencimento);
+  ~Boleto();
+  
+  int getVencimento();
+  bool getVenceu();
+  void relatorioDeCategoria();
+};
 #endif
