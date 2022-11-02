958ed5b1ad75965897381e58b0760697c30bb271
matbs <mateusbs20222@gmail.com>
2022-11-02 09:29:50 -0300

    Update categoria.hpp
    Atualização do HPP categoria ---- Erro de Include Path

--- include/categoria.hpp
+++ include/categoria.hpp
@@ -1,3 +1,18 @@
 #ifndef CATEGORIA_H
 #define CATEGORIA_H
+
+#include <string>
+
+    class Categoria(){
+        string nome;
+        double orcamento;
+        double gastoTotal;
+        bool limite;
+    public :
+        void setGasto(double valor);
+        double getGasto();
+        bool statusLimite();
+        void alteraCaixa(double valor);
+        void relatorioDeCategoria();
+    }
 #endif
