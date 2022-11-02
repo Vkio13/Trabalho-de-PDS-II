36cf68def770e8aae5cfd58a3702b65e0f87ab59
matbs <mateusbs20222@gmail.com>
2022-11-02 09:39:27 -0300

    Update relatorio.hpp
    Atualização class Relatorio

--- include/relatorio.hpp
+++ include/relatorio.hpp
@@ -1,3 +1,11 @@
 #ifndef RELATORIO_H
 #define RELATORIO_H
+#include "categoria.hpp"
+
+    class Relatorio{
+        list<Categoria> categorias;
+    public :
+        void relatorioMensal();
+        void relatorioGeral();
+    };
 #endif
