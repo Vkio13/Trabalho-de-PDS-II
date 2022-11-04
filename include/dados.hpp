ff8a0bc329f5582d9efedcb904b33c2c0ba9b4cc
GabrielCaldeiraBicalho <69369861+GabrielCaldeiraBicalho@users.noreply.github.com>
2022-11-03 21:55:26 -0300

    Adição das funções e variáveis principais

--- include/dados.hpp
+++ include/dados.hpp
@@ -1,3 +1,19 @@
 #ifndef DADOS_H
 #define DADOS_H
+#include <string>
+  class Dados{
+    private:
+      string historico ="usuario.txt";
+    public:
+      void imprimeGastosMensal(int mes);
+      void imprimeGastosTodos();
+      double somaGastosMes(int mes);
+      double somaGastosTotal();
+      double somaGastosCategoria(string categoria);
+      double somaGastosCategoriaMensal(string categoria, int mes);
+      double somaEntradas();
+      double somaEntradas(int mes);
+      void imprimeCategoriaTotal(string categoria);
+      void imprimeCategoriaMensal(string categoria, int mes);
+  };
 #endif
