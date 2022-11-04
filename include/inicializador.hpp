5f7877a689c6da8cb1caa5dbcee99e16eb4357a7
GabrielCaldeiraBicalho <69369861+GabrielCaldeiraBicalho@users.noreply.github.com>
2022-11-03 21:51:23 -0300

    Criação das funções e variáveis principais

--- include/inicializador.hpp
+++ include/inicializador.hpp
@@ -1,6 +1,17 @@
 #ifndef INICIALIZADOR_H
 #define INICIALIZADOR_H
 #include <list>
+#include "categoria.hpp"
 class Inicializador {
+    private:
+        list<Categoria> categorias;
+        double entradas,investimento,gastos;
+    public :
+        double GetEntradas();
+        double GetGastos();
+        list<Categoria> GetCategorias();
+        void carregaEntradas();
+        void carregaGastos();
+        void carregaCategorias();
 };
 #endif
