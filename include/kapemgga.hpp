07b100ae433d8cca2ae48b7a1d4e552f27bea753
Vkio13 <111925541+Vkio13@users.noreply.github.com>
2022-11-18 15:45:12 -0300

    Update kapemgga.hpp
    consertei os std que eu tinha esquecido em alguns pontos e adicionei legendas pra facilitar no doxygen

--- include/kapemgga.hpp
+++ include/kapemgga.hpp
@@ -1,26 +1,52 @@
 #ifndef KAPEMGGA_HPP
 #define KAPEMGGA_HPP
+
 #include <list>
 #include <string>
 
 class Kapemgga{
   private:
-  list<Categoria>_categorias;
+  std::list<Categoria>_categorias;
   Caixa _caixa;
   public:
-  Kapemgga(list<Categoria> categorias, Caixa caixa);
+  //Construtor do Kappemgga.
+  Kapemgga(std::list<Categoria> categorias, Caixa caixa);
+
+  //Destrutor do Kappemga.
   ~Kapemgga();
+  
+  //Função para imprimir todas as categorias que o usuário tem.
   void imprimeCategorias();
-  void adicionaCategoria(string nome, double orcamento);
-  void removeCategoria(string nome);
-  void novaReceita(string descricao, double valor);
+  
+  //Função que permite o usuário criar novas categorias.
+  void adicionaCategoria(std::string nome, double orcamento);
+  
+  //Função que permite o usuário excluir alguma categoria.
+  void removeCategoria(std::string nome);
+  
+  //Função que permite o usuário somar um valor x ao que ele já tem disponível, e especificar a origem do dinheiro.
+  void novaReceita(std::string descricao, double valor);
+  
+  //Função que mostra os limites de gastos delimitados a cada categoria (?).
   void imprimeLimites();
-  void novoGasto(string categoria, double valor)
-  void pagaBoleto(string nome);
+  
+  //Função responsável por realizar um gasto dentro de uma categoria especificada.
+  void novoGasto(std::string categoria, double valor)
+  
+  //Função para pagar determinado boleto, de acordo com seu nome.
+  void pagaBoleto(std::string nome);
+  
+  //Função responsável por dar o comando de fechar o mês.
   void encerraMes();
+  
+  //Função que permite ao usuário emitir um relatório mensal de gastos e receitas recebidas.
   void relatorioMensal(int mes);
+  
+  //Função que permite ao usuário emitir um relatório de gastos e receitas recebidas desde que começou a usar o sistema.
   void relatorioGeral();
-  void relatorioAtual()
+  
+  //Função que permite ao usuário emitir um relatório do dia atual.
+  void relatorioAtual();
 
 
 #endif
