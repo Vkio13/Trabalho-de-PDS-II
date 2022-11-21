00cda73c6f112d7bb8abbaf5751f9f6ac123ef06
GabrielCaldeiraBicalho <69369861+GabrielCaldeiraBicalho@users.noreply.github.com>
2022-11-21 19:49:42 -0300

    Inserção do Doxygen

--- include/relatorio.hpp
+++ include/relatorio.hpp
@@ -1,16 +1,50 @@
 #ifndef RELATORIO_H
 #define RELATORIO_H
+/**
+ * @file relatorio.hpp
+ * @brief Classe onde estão arma
+ * @version 1.0
+ * @date 2022-11-21
+ * @details TAD com função principal de 
+ * conter um relatório com todos os dados
+ * sendo eles os gastos e outros inseridos
+ * em Dados.hpp
+ * 
+ * @copyright GNU General Public License 
+ * 
+ */
 #include "../include/categoria.hpp"
 
     class Relatorio{
         std::list<Categoria> _categorias;
     public :
-    //Construtor
+/**
+         * @brief Constrói o objeto Relatorio.
+         * 
+         * @details Recebe as categorias e as insere
+         * dentro do relatório.
+         *
+         * @param categorias Lista do tipo Categoria
+         * contendo todas as categorias registradas
+         * e seus dados.
+         *
+         */        
         Relatorio(std::list<Categoria> categorias);
-    //Destrutor
+/**
+         * @brief Destrutor do objeto Relatorio.
+         *
+         */ 
         ~Relatorio();
-    //Métodos
+ /**
+         * @brief Printa o relatório mensal.
+         * 
+         * 
+         */
         void relatorioMensal();
+ /**
+         * @brief Printa o relatório geral.
+         * 
+         */
         void relatorioGeral();
     };
 #endif
