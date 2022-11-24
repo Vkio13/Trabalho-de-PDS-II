fdc0ee5f02e4b79a5bb01a156c290b9c1e940577
GabrielCaldeiraBicalho <69369861+GabrielCaldeiraBicalho@users.noreply.github.com>
2022-11-24 15:49:47 -0300

    Inserção do Doxygen

--- include/boleto.hpp
+++ include/boleto.hpp
@@ -1,20 +1,65 @@
 #ifndef BOLETO_H
 #define BOLETO_H
+/**
+ * @file boleto.hpp
+ * @brief Subclasse Boleto 
+ * @version 1.0
+ * @date 2022-11-24
+ * @details TAD com função principal de 
+ * conter a data de vencimento de um 
+ * determinado boleto
+ * 
+ * @copyright GNU General Public License 
+ * 
+ */
 #include "categoria.hpp"
-
+/**
+         * @brief Classe contendo informações básicas
+         * de um boleto
+         *
+         * @details Armazena informações basicas de um boleto
+         *
+         */   
 class Boleto : public Categoria {
     private:
+    /**
+         * @brief Valor do vencimento do boleto.
+         */
         int _vencimento;
+    /**
+         * @brief bool dizendo se o boleto venceu ou não
+         */
         bool _pago;
     
     public:
-        //Construtor
+        /**
+         * @brief Constrói o objeto Boleto.
+         *
+         * @param nome Nome do boleto
+         *
+         * @param valor Valor do boleto
+         * 
+         * @param vencimento Data do vencimento do boleto
+         *
+         */ 
         Boleto(std::string nome, int valor, int vencimento);
-        //Destrutor
+        /**
+         * @brief Destrutor da classe Boleto
+         */
         ~Boleto();
-        //Métodos
+          /**
+         * @brief efetua o pagamento do boleto
+         */
         void pagaBoleto();
+        /**
+         * @brief função que diz se o boleto venceu
+         *
+         * @return retorna se venceu ou não
+         */
         bool venceu();
+        /**
+         * @brief apresenta um relatorio da categoria
+         */
         void relatorioDeCategoria() override;
 };
 #endif
