e227fee41aa732c3d5cd581a42fe8f8fc119f67e
gustavolc12 <gustavolc09@gmail.com>
2022-11-25 19:10:01 -0300

    .

--- main.cpp
+++ main.cpp
@@ -1,16 +1,8 @@
 #include "include/dados.hpp"
 #include "include/caixa.hpp"
 int main(){
-    Dados d;
-    d.adicionaGasto(6,"Bebida","Stella-Artois");
-    d.adicionaGasto(6,"Bebida","Suco");
-    d.adicionaGasto(6,"Bebida","Agua de coco3");
-    d.adicionaGasto(6,"Bebida","Refrigerante4");
-    d.adicionaGasto(6,"Bebida com alcool","Vodka");
-    d.imprimeCategorias();
-    d.imprimeGastosTodos();
-    d.imprimeGastosCategoriaTotal("Bebida");
-    d.deleteGasto(4);
-    Caixa nada;
-    return 0;
+    int op;
+    while(std::cin>>op){
+        
+    }
 }
