6bdc90127aba30dee66546a1c9d8104945f69761
matbs <mateusbs20222@gmail.com>
2022-11-27 11:45:32 -0300

    Testes
    Esqueleto dos testes

---
+++ tests/dados_test.cpp
@@ -0,0 +1,15 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../third_party/doctest.h"
+#include "../include/dados.hpp"
+
+TEST_CASE("Testando a função"){
+    SUBCASE("Caso Positivo"){
+
+    }
+    SUBCASE("Caso Negativo"){
+
+    }
+    SUBCASE("Caso Especial"){
+
+    }
+}
