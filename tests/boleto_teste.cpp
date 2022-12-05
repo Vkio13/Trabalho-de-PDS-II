04959fd7513e4d8b3b8af6f43f6241d97cb647f8
gustavolc12 <gustavolc09@gmail.com>
2022-12-05 20:09:20 -0300

    Testes de Boleto

--- tests/boleto_teste.cpp
+++ tests/boleto_teste.cpp
@@ -9,4 +9,17 @@ TEST_CASE("Testando as exceções"){
         CHECK_THROWS(sistemas.adicionaBoleto(Boleto("Teste de Exceçao",100,32,12)));
         CHECK_THROWS(sistemas.adicionaBoleto(Boleto("Teste de Exceçao",-100,30,12)));
         CHECK_THROWS(sistemas.adicionaBoleto(Boleto("UmValorComMaisDeVinteCaracteresParaOTesteDeExcecao",100,30,12)));
+
+        Boleto b1("Conta",10,30,12);
+        Boleto b2("Bola",200,30,1);
+        SUBCASE("Venceu"){
+                CHECK_EQ(b1.venceu(),false);
+                CHECK_EQ(b2.venceu(),true);
+        }
+        
+        SUBCASE("Pago"){
+                b2.pagaBoleto();
+                CHECK_EQ(b1.get_pago(),false);
+                CHECK_EQ(b2.get_gasto(),true);
+        }        
 }
